3c0ae88c192071c1ac9a814e8073a00f16d05b56
红三七 <hongsanqi@myweimai.com>
2021-09-30 15:58:14 +0800

    move function associated with near connection into NearConnection.js

--- frontend/src/modules/confirm/pages/home.js
+++ frontend/src/modules/confirm/pages/home.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import * as nearAPI from 'near-api-js'
+import NearConnection from '../../../pages/NearConnection';
 
-const ContractName = 'contract003.testnet';
-const { utils } = nearAPI;
 
 export default class Home extends React.Component {
 
@@ -12,20 +10,19 @@ export default class Home extends React.Component {
             authResult : ""
         }
         let transactionHashes = this.getTransactionHashes()
-        this._initNear().then(() => {
+        this.nearConnection = new NearConnection()
+
+        this.nearConnection.init().then(() => {
             if(transactionHashes==""){
                 this.authRequest()
             }else{
-                this.getTransactionStatus(transactionHashes)
+                this.actionAfterConfirm(transactionHashes)
             }
         });
     }
     
-    async getTransactionStatus(hax){
-        const provider = new nearAPI.providers.JsonRpcProvider(
-            "https://archival-rpc.testnet.near.org"
-          );
-        const result = await provider.txStatus(hax, this._accountId);
+    async actionAfterConfirm(hax){
+        const result = await this.nearConnection.getTransactionStatus(hax)
         if('SuccessValue' in result['status']){
             window.opener.actionAfterConfirm({
                 hover: {
@@ -65,53 +62,10 @@ export default class Home extends React.Component {
         return ""
     }
 
-    async _initNear() {
-        const nearConfig = {
-          networkId: 'default',
-          nodeUrl: 'https://rpc.testnet.near.org',
-          contractName: ContractName,
-          walletUrl: 'https://wallet.testnet.near.org',
-        };
-        const keyStore = new nearAPI.keyStores.BrowserLocalStorageKeyStore();
-        // near connection
-        const near = await nearAPI.connect(Object.assign({ deps: { keyStore } }, nearConfig));
-        this._keyStore = keyStore;
-        this._nearConfig = nearConfig;
-        this._near = near;
-    
-        this._walletConnection = new nearAPI.WalletConnection(near, ContractName);
-        this._accountId = this._walletConnection.getAccountId();
-        this._account = this._walletConnection.account();
-    
-        this._contract = new nearAPI.Contract(this._account, ContractName, {
-          viewMethods: ['questionCount', 'answerCount','showQuestion', 'showAnswer', 'listMyAskedQuestions', 'listUnAnsweredQuestions', 'listAnsweredQuestions', 'listAllRelQuestions'],
-          changeMethods: ['createQuestion', 'answerQuestion', 'claimReward'],
-        });
-        this._keySource = null
-        if (this._accountId) {
-          const _accessKey = await this._account.getAccessKeys();
-          for(var i=0;i<_accessKey.length;i++){
-            if(_accessKey[i].access_key.permission==='FullAccess') continue;
-            if(_accessKey[i].access_key.permission.FunctionCall.receiver_id===ContractName){
-              this._keysource = _accessKey[i].public_key;
-              break;
-            }
-          }
-        }
-      }
-
     async authRequest(){
         var info = this.parentInfo()
-        console.log(info)
         const totalNear = info['near']
-        await this._contract.createQuestion({
-            id: info['id'],
-            title:info['title'],
-            content:info['content'],
-            repliers: info['repliers'],
-            moneys: info['moneys'],
-            lock: info['lock']
-        }, 30000000000000, utils.format.parseNearAmount(`${totalNear}`))
+        await this.nearConnection.createQuestion(info, totalNear)
     }
 
     parentInfo(){
